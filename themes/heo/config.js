9a418eb7f0fc3b5fdf855feb819642e68240ff06
Peter <129477152+miniLQ@users.noreply.github.com>
2023-08-21 09:52:48 +0800

    Update config.js
    打开社群功能

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -98,11 +98,11 @@ const CONFIG = {
     }
   ],
 
-  SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
-  SOCIAL_CARD_TITLE_1: '交流频道',
+  SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  SOCIAL_CARD_TITLE_1: 'Discord交流频道',
   SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   SOCIAL_CARD_TITLE_3: '点击加入社群',
-  SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  SOCIAL_CARD_URL: 'https://discord.com/channels/1142999348972621914/1142999348972621917',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
